a5ed324c19ee3f7c8361bd69574c7299e0c40ea4
anthonyslatermusic <anthony.slater111@gmail.com>
2020-02-02 19:34:48 +0200

    4 files added, 4 deleted

--- src/app/slider-example.component.ts
+++ src/app/slider-example.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-slider-example',
+  selector: 'slider-example',
   templateUrl: './slider-example.component.html',
   styleUrls: ['./slider-example.component.css']
 })
